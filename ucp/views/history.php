4ee8a441d3d14c3f80c679988fe0dfdc28535d3f
Andrew Nagy <andrew.nagy@the159.com>
2015-01-29 18:13:03 -0500

    update history file

--- ucp/views/history.php
+++ ucp/views/history.php
@@ -21,39 +21,37 @@
 				<tr class="message-header">
 					<th class="noclick"></th>
 					<th data-type="date"><?php echo _('Last Message Date')?> <i class="fa fa-chevron-<?php echo ($order == 'desc' && $orderby == 'date') ? 'down' : 'up'?> <?php echo ($orderby == 'date') ? '' : 'hidden'?>"></i></th>
-					<th data-type="from"><?php echo _('From')?> <i class="fa fa-chevron-<?php echo ($order == 'desc' && $orderby == 'from') ? 'down' : 'up'?> <?php echo ($orderby == 'from') ? '' : 'hidden'?>"></i></th>
+					<th data-type="from"><?php echo _('DID')?> <i class="fa fa-chevron-<?php echo ($order == 'desc' && $orderby == 'from') ? 'down' : 'up'?> <?php echo ($orderby == 'from') ? '' : 'hidden'?>"></i></th>
 					<th data-type="to"><?php echo _('To')?> <i class="fa fa-chevron-<?php echo ($order == 'desc' && $orderby == 'to') ? 'down' : 'up'?> <?php echo ($orderby == 'to') ? '' : 'hidden'?>"></i></th>
 					<th class="noclick"><?php echo _('Controls')?></th>
 				</tr>
 			</thead>
 			<tbody>
 			<?php if(!empty($messages)) {?>
-				<?php foreach($messages as $from => $conversation){?>
-					<?php foreach($conversation as $to => $messages) { ?>
-						<?php $c = count($messages); $last = $messages[0]; $prev = 0;?>
-						<tr class="sms-message" data-from="<?php echo $from?>" data-to="<?php echo $to?>">
+				<?php foreach($messages as $conversation){?>
+						<?php $c = count($conversation['messages']); $last = $conversation['messages'][0]; $prev = 0;?>
+						<tr class="sms-message" data-from="<?php echo $conversation['from']?>" data-to="<?php echo $conversation['to']?>">
 							<td class=""></td>
 							<td class=""><?php echo $last['tx_rx_datetime']?></td>
-							<td class=""><?php echo \UCP\UCP::create()->Modules->Sms->replaceDIDwithDisplay($from)?></td>
-							<td class=""><a onclick="UCP.Modules.Sms.startChat('<?php echo $from?>','<?php echo $to?>')"><?php echo \UCP\UCP::create()->Modules->Sms->replaceDIDwithDisplay($to) ?></a></td>
-							<td class="actions"><a><i class="fa fa-eye" data-id="<?php echo $from?><?php echo $to?>"></i></a><a><i class="fa fa-trash-o"></i></a></td>
+							<td class=""><?php echo $conversation['from']?></td>
+							<td class=""><a onclick="UCP.Modules.Sms.startChat('<?php echo $conversation['from']?>','<?php echo $conversation['to']?>')"><?php echo $conversation['prettyto'] ?></a></td>
+							<td class="actions"><a><i class="fa fa-eye" data-id="<?php echo $conversation['from']?><?php echo $conversation['to']?>"></i></a><a><i class="fa fa-trash-o"></i></a></td>
 						</tr>
-						<tr class="sms-message-body-container" id="<?php echo $from?><?php echo $to?>-messages">
+						<tr class="sms-message-body-container" id="<?php echo $conversation['from']?><?php echo $conversation['to']?>-messages">
 							<td class=""></td>
 							<td class="" colspan="4">
 								<div class="sms-message-body">
-									<?php foreach($messages as $message) { ?>
+									<?php foreach($conversation['messages'] as $message) { ?>
 										<?php if($prev != date('j',$message['utime'])) {?>
 											<div class="full-date"><?php echo date('m/d/Y',$message['utime'])?></div>
 											<hr>
 										<?php } ?>
-										<span class="date">[<?php echo date('H:i',$message['utime'])?>]</span> <span class="name"><?php echo ($from == $message['from']) ? _('Me') : \UCP\UCP::create()->Modules->Sms->replaceDIDwithDisplay($message['from'])?></span>: <?php echo $message['body']?><br/>
+										<span class="date">[<?php echo date('H:i',$message['utime'])?>]</span> <span class="name"><?php echo ($conversation['from'] == $message['from']) ? _('Me') : \UCP\UCP::create()->Modules->Sms->replaceDIDwithDisplay($message['from'])?></span>: <?php echo $message['body']?><br/>
 										<?php $prev = date('j',$message['utime']);?>
 									<?php } ?>
 								</div>
 							</td>
 						</tr>
-					<?php } ?>
 				<?php }?>
 			<?php } else { ?>
 				<tr id="no-messages" class="sms-message">
